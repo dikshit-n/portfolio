c9bbdf9b9498e3c3957097db12a0d69d2c57c7ca
dikshit-n <62920150+dikshit-n@users.noreply.github.com>
2022-11-24 10:32:56 +0530

    customization from data file

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,85 +5,15 @@ import { Row, Col } from "reactstrap";
 // import Progress from "react-circle-progress-bar";
 // import Projects from "./Projects/Projects";
 import Skill from "../UI/Skill";
-import LinkedinIcon from "../../assets/social-icons/linkedin.png";
-import GithubIcon from "../../assets/social-icons/github.png";
-import ResumeIcon from "../../assets/social-icons/resume.png";
-import ReactLogo from "../../assets/img/react.png";
-import NextLogo from "../../assets/img/next.png";
-import ReduxLogo from "../../assets/img/redux.png";
-import ReactQueryLogo from "../../assets/img/react-query.png";
-import HTMLLogo from "../../assets/img/html.png";
-import CSSLogo from "../../assets/img/css.png";
-import SCSSLogo from "../../assets/img/scss.png";
-import GitLogo from "../../assets/img/git.png";
-import MuiLogo from "../../assets/img/mui.png";
-import ReactstrapLogo from "../../assets/img/reactstrap.png";
-import BootstrapLogo from "../../assets/img/bootstrap.png";
-import JSLogo from "../../assets/img/js.png";
-import TSLogo from "../../assets/img/ts.png";
-import EmergeAbility from "../../assets/img/emergeability.png";
-import EmergeAbilityWebsite from "../../assets/img/emergeability-website.jpg";
 import ProgramingLanguageCard from "../UI/ProgramingLanguageCard";
 import Project from "../UI/Project";
 import Experience from "./Experience";
-
-const social = {
-  linkedin: {
-    image: LinkedinIcon,
-    link: "https://www.linkedin.com/in/dikshitkumar-n-b66177199/",
-  },
-  github: {
-    image: GithubIcon,
-    link: "https://github.com/dikshit-n?tab=repositories",
-  },
-  resume: {
-    image: ResumeIcon,
-    link: "https://dikshit-n.github.io/resume/Resume.pdf",
-  },
-};
-
-const projects = [
-  {
-    name: "EmergeAbility",
-    link: "http://emergeability.org/",
-    image: EmergeAbility,
-    embedLink: "http://emergeability.org/",
-    previewImage: EmergeAbilityWebsite,
-  },
-];
-
-const skills = {
-  react: { link: "https://reactjs.org/", image: ReactLogo },
-  next: { link: "https://nextjs.org/", image: NextLogo },
-  redux: { link: "https://redux.js.org/", image: ReduxLogo },
-  reactQuery: {
-    link: "https://react-query.tanstack.com/",
-    image: ReactQueryLogo,
-  },
-  html: {
-    link: "https://www.w3schools.com/html/html_intro.asp",
-    image: HTMLLogo,
-  },
-  css: {
-    link: "https://www.w3schools.com/css/css_intro.asp",
-    image: CSSLogo,
-  },
-  scss: { link: "https://sass-lang.com/", image: SCSSLogo },
-  git: { link: "https://github.com/", image: GitLogo },
-  mui: { link: "https://mui.com/", image: MuiLogo },
-  reactstrap: { link: "https://reactstrap.github.io/", image: ReactstrapLogo },
-  bootstrap: { link: "https://getbootstrap.com/", image: BootstrapLogo },
-};
-
-const programingLanguage = {
-  javascript: { link: "https://www.javascript.com/", image: JSLogo },
-  typescript: { link: "https://www.typescriptlang.org/", image: TSLogo },
-};
+import { myDetails } from "../../data/static";
 
 const HomePage = (props) => {
   const gotoEmail = (e) => {
     e.preventDefault();
-    window.open("mailto:dikshitkumarn@gmail.com");
+    window.open(`mailto:${myDetails.personal.contact.email}`);
   };
 
   return (
@@ -97,17 +27,17 @@ const HomePage = (props) => {
           />
         </div>
         <div className="Intro-container">
-          <h1>Hello, I am Dikshitkumar</h1>
-          <h4>Front End Developer</h4>
+          <h1>Hello, I am {myDetails.personal.name}</h1>
+          <h4>{myDetails.personal.designation}</h4>
           <div className="social-media">
-            <a href={social.linkedin.link} target="_blank">
-              <img src={social.linkedin.image} alt="linkedin" />
+            <a href={myDetails.social.linkedin.link} target="_blank">
+              <img src={myDetails.social.linkedin.image} alt="linkedin" />
             </a>
-            <a href={social.github.link} target="_blank">
-              <img src={social.github.image} alt="github" />
+            <a href={myDetails.social.github.link} target="_blank">
+              <img src={myDetails.social.github.image} alt="github" />
             </a>
-            <a href={social.resume.link} target="_blank">
-              <img src={social.resume.image} alt="resume" />
+            <a href={myDetails.social.resume.link} target="_blank">
+              <img src={myDetails.social.resume.image} alt="resume" />
             </a>
           </div>
         </div>
@@ -127,15 +57,7 @@ const HomePage = (props) => {
           </Col>
           <Col lg="6" md="12" sm="12" xs="12" className="about-me">
             <ul>
-              <li>
-                I develop innovative websites with attractive designs and great
-                user experience.
-              </li>
-              <li>
-                For past <strong>1.8 years</strong>, I have been part of
-                contract based projects, office work and as a freelancer.
-              </li>
-              <li>Still practicing to perfectly master my skills.</li>
+              {myDetails.personal.aboutMe.map((el, index) => <li key={index}>{el}</li>)}
             </ul>
           </Col>
         </Row>
@@ -151,78 +73,78 @@ const HomePage = (props) => {
               <Col sm="12" md="4">
                 <Skill
                   name="React JS"
-                  link={skills.react.link}
-                  image={skills.react.image}
+                  link={myDetails.skills.react.link}
+                  image={myDetails.skills.react.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="Next JS"
-                  link={skills.next.link}
-                  image={skills.next.image}
+                  link={myDetails.skills.next.link}
+                  image={myDetails.skills.next.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="Redux JS"
-                  link={skills.redux.link}
-                  image={skills.redux.image}
+                  link={myDetails.skills.redux.link}
+                  image={myDetails.skills.redux.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="React query"
-                  link={skills.reactQuery.link}
-                  image={skills.reactQuery.image}
+                  link={myDetails.skills.reactQuery.link}
+                  image={myDetails.skills.reactQuery.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="HTML"
-                  link={skills.html.link}
-                  image={skills.html.image}
+                  link={myDetails.skills.html.link}
+                  image={myDetails.skills.html.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="CSS"
-                  link={skills.css.link}
-                  image={skills.css.image}
+                  link={myDetails.skills.css.link}
+                  image={myDetails.skills.css.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="SCSS"
-                  link={skills.scss.link}
-                  image={skills.scss.image}
+                  link={myDetails.skills.scss.link}
+                  image={myDetails.skills.scss.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="Git / Github"
-                  link={skills.git.link}
-                  image={skills.git.image}
+                  link={myDetails.skills.git.link}
+                  image={myDetails.skills.git.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="Material UI"
-                  link={skills.mui.link}
-                  image={skills.mui.image}
+                  link={myDetails.skills.mui.link}
+                  image={myDetails.skills.mui.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="Reactstrap"
-                  link={skills.reactstrap.link}
-                  image={skills.reactstrap.image}
+                  link={myDetails.skills.reactstrap.link}
+                  image={myDetails.skills.reactstrap.image}
                 />
               </Col>
               <Col sm="12" md="4">
                 <Skill
                   name="Bootstrap"
-                  link={skills.bootstrap.link}
-                  image={skills.bootstrap.image}
+                  link={myDetails.skills.bootstrap.link}
+                  image={myDetails.skills.bootstrap.image}
                 />
               </Col>
             </Row>
@@ -242,13 +164,13 @@ const HomePage = (props) => {
           >
             <ProgramingLanguageCard
               name="Javascript"
-              image={programingLanguage.javascript.image}
-              link={programingLanguage.javascript.link}
+              image={myDetails.programingLanguage.javascript.image}
+              link={myDetails.programingLanguage.javascript.link}
             />
             <ProgramingLanguageCard
               name="Typescript"
-              image={programingLanguage.typescript.image}
-              link={programingLanguage.typescript.link}
+              image={myDetails.programingLanguage.typescript.image}
+              link={myDetails.programingLanguage.typescript.link}
             />
           </Col>
         </Row>
@@ -260,7 +182,7 @@ const HomePage = (props) => {
             <BlueLine />
           </div>
           <div className="projects-container">
-            {projects.map((el, index) => (
+            {myDetails.projects.map((el, index) => (
               <Project {...el} key={index} />
             ))}
           </div>
@@ -290,23 +212,23 @@ const HomePage = (props) => {
             style={{ cursor: "pointer" }}
           >
             <h4>Email</h4>
-            <p>dikshitkumarn@gmail.com</p>
+            <p>{myDetails.personal.contact.email}</p>
           </Col>
           <Col lg="4" md="12" sm="12" xs="12">
             <h4>Contact Number</h4>
-            <p>9600714338</p>
+            <p>{myDetails.personal.contact.number}</p>
           </Col>
           <Col lg="4" md="12" sm="12" xs="12">
             <h4>Social Links</h4>
             <div className="contact-social-media">
-              <a href={social.linkedin.link} target="_blank">
-                <img src={social.linkedin.image} alt="linkedin" />
+              <a href={myDetails.social.linkedin.link} target="_blank">
+                <img src={myDetails.social.linkedin.image} alt="linkedin" />
               </a>
-              <a href={social.github.link} target="_blank">
-                <img src={social.github.image} alt="github" />
+              <a href={myDetails.social.github.link} target="_blank">
+                <img src={myDetails.social.github.image} alt="github" />
               </a>
-              <a href={social.resume.link} target="_blank">
-                <img src={social.resume.image} alt="resume" />
+              <a href={myDetails.social.resume.link} target="_blank">
+                <img src={myDetails.social.resume.image} alt="resume" />
               </a>
             </div>
           </Col>
---
+++ src/data/static.js
@@ -0,0 +1,84 @@
+
+import LinkedinIcon from "../assets/social-icons/linkedin.png";
+import GithubIcon from "../assets/social-icons/github.png";
+import ResumeIcon from "../assets/social-icons/resume.png";
+import ReactLogo from "../assets/img/react.png";
+import NextLogo from "../assets/img/next.png";
+import ReduxLogo from "../assets/img/redux.png";
+import ReactQueryLogo from "../assets/img/react-query.png";
+import HTMLLogo from "../assets/img/html.png";
+import CSSLogo from "../assets/img/css.png";
+import SCSSLogo from "../assets/img/scss.png";
+import GitLogo from "../assets/img/git.png";
+import MuiLogo from "../assets/img/mui.png";
+import ReactstrapLogo from "../assets/img/reactstrap.png";
+import BootstrapLogo from "../assets/img/bootstrap.png";
+import JSLogo from "../assets/img/js.png";
+import TSLogo from "../assets/img/ts.png";
+import EmergeAbility from "../assets/img/emergeability.png";
+import EmergeAbilityWebsite from "../assets/img/emergeability-website.jpg";
+
+export const myDetails = {
+    personal: {
+        name: 'Dikshitkumar N',
+        designation: 'Front End Developer',
+        aboutMe: [
+            'I develop innovative websites with attractive designs and great user experience.',
+            'For past 1.8 years, I have been part of contract based projects, office work and as a freelancer.',
+            'Still practicing to perfectly master my skills.'
+        ],
+        contact: {
+            email: 'dikshitkumarn@gmail.com',
+            number: '9600714338',
+        }
+    },
+    social: {
+        linkedin: {
+        image: LinkedinIcon,
+        link: "https://www.linkedin.com/in/dikshitkumar-n-b66177199/",
+        },
+        github: {
+        image: GithubIcon,
+        link: "https://github.com/dikshit-n?tab=repositories",
+        },
+        resume: {
+        image: ResumeIcon,
+        link: "https://dikshit-n.github.io/resume/Resume.pdf",
+        },
+    },
+    projects: [
+        {
+        name: "EmergeAbility",
+        link: "http://emergeability.org/",
+        image: EmergeAbility,
+        embedLink: "http://emergeability.org/",
+        previewImage: EmergeAbilityWebsite,
+        },
+    ],
+    skills: {
+        react: { link: "https://reactjs.org/", image: ReactLogo },
+        next: { link: "https://nextjs.org/", image: NextLogo },
+        redux: { link: "https://redux.js.org/", image: ReduxLogo },
+        reactQuery: {
+        link: "https://react-query.tanstack.com/",
+        image: ReactQueryLogo,
+        },
+        html: {
+        link: "https://www.w3schools.com/html/html_intro.asp",
+        image: HTMLLogo,
+        },
+        css: {
+        link: "https://www.w3schools.com/css/css_intro.asp",
+        image: CSSLogo,
+        },
+        scss: { link: "https://sass-lang.com/", image: SCSSLogo },
+        git: { link: "https://github.com/", image: GitLogo },
+        mui: { link: "https://mui.com/", image: MuiLogo },
+        reactstrap: { link: "https://reactstrap.github.io/", image: ReactstrapLogo },
+        bootstrap: { link: "https://getbootstrap.com/", image: BootstrapLogo },
+    },
+    programingLanguage: {
+        javascript: { link: "https://www.javascript.com/", image: JSLogo },
+        typescript: { link: "https://www.typescriptlang.org/", image: TSLogo },
+    },
+}
